5aff2290bf8e838db39b3bba372407dfd77a1cfc
herq-ules <heruoqingvivien@gmail.com>
2023-05-29 16:44:13 +0800

    uploadfilemodal

--- src/components/chat/UploadFileModal.js
+++ src/components/chat/UploadFileModal.js
@@ -27,143 +27,148 @@ const Button = styled.button`
 `;
 
 const UploadFileModal = (props) => {
-    // Create new plugin instance
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
-    // for onchange event
-    const [selectPdfFile, setSelectPdfFile] = useState(null);
-    const [pdfFileError, setPdfFileError] = useState("");
-    // for submit event
-    const [viewPdf, setViewPdf] = useState(null);
-    // onchange event
-    const fileObj = ["application/pdf"];
-    const handleFileChange = (e) => {
-      let selectedFile = e.target.files[0];
-      if (selectedFile) {
-        if (selectedFile && fileObj.includes(selectedFile.type)) {
-          let reader = new FileReader();
-          reader.readAsDataURL(selectedFile);
-          reader.onloadend = (e) => {
-            setSelectPdfFile(e.target.result);
-            setPdfFileError("");
-          };
-        } else {
-          setSelectPdfFile(null);
-          setPdfFileError("Please select valid pdf file");
-        }
+  // Create new plugin instance
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  // for onchange event
+  const [selectPdfFile, setSelectPdfFile] = useState(null);
+  const [pdfFileError, setPdfFileError] = useState("");
+  // for submit event
+  const [viewPdf, setViewPdf] = useState(null);
+  // onchange event
+  const fileObj = ["application/pdf"];
+  const handleFileChange = (e) => {
+    let selectedFile = e.target.files[0];
+    if (selectedFile) {
+      if (selectedFile && fileObj.includes(selectedFile.type)) {
+        let reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend = (e) => {
+          setSelectPdfFile(e.target.result);
+          setPdfFileError("");
+        };
       } else {
-        alert("select pdf file");
+        setSelectPdfFile(null);
+        setPdfFileError("Please select valid pdf file");
       }
-    };
-    // form submit
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (selectPdfFile !== null) {
-        setViewPdf(selectPdfFile);
-      } else {
-        setViewPdf(null);
-      }
-    };
+    } else {
+      alert("select pdf file");
+    }
+  };
+  // form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (selectPdfFile !== null) {
+      setViewPdf(selectPdfFile);
+    } else {
+      setViewPdf(null);
+    }
+  };
 
-    const hiddenFileInput = useRef(null);
+  const hiddenFileInput = useRef(null);
 
-    const handleClick = (event) => {
-      hiddenFileInput.current.click();
-    };
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
 
-//   const [files, setFiles] = useState([]);
+  //   const [files, setFiles] = useState([]);
 
-//   const handleDrop = (event) => {
-//     event.preventDefault();
-//     const { files } = event.dataTransfer;
-//     if (files.length > 0) {
-//       setFiles([...files]);
-//     }
-//   };
+  //   const handleDrop = (event) => {
+  //     event.preventDefault();
+  //     const { files } = event.dataTransfer;
+  //     if (files.length > 0) {
+  //       setFiles([...files]);
+  //     }
+  //   };
 
-//   const handleDragOver = (event) => {
-//     event.preventDefault();
-//   };
+  //   const handleDragOver = (event) => {
+  //     event.preventDefault();
+  //   };
 
-//   const handleDragStart = (event) => {
-//     event.dataTransfer.setData("text/plain", event.target.id);
-//   };
+  //   const handleDragStart = (event) => {
+  //     event.dataTransfer.setData("text/plain", event.target.id);
+  //   };
 
-    return (
-      <Modal onClose={props.onClose}>
-        <div className="container">
-          <h2> Upload a PDF file</h2>
-          <form className="form-group" onSubmit={handleSubmit}>
-            <Button onClick={handleClick}> Choose File</Button>
-            <input
-              type="file"
-              ref={hiddenFileInput}
-              style={{ display: "none" }}
-              className="form-control"
-              required
-              onChange={handleFileChange}
-            />
-            {pdfFileError && <div className="error-msg">{pdfFileError}</div>}
-            <br></br>
-            <Button type="submit">Upload</Button>
-            {/* <button type="submit" className="btn btn-success btn-lg">
-              UPLOAD
-            </button> */}
-          </form>
-          <h2>View PDF</h2>
-          <div className="pdf-container">
-            {/* show pdf conditionally (if we have one)  */}
-            {viewPdf && (
-              <>
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
-                  <Viewer
-                    fileUrl={viewPdf}
-                    plugins={[defaultLayoutPluginInstance]}
-                  />
-                </Worker>
-              </>
-            )}
+  return (
+    <Modal onClose={props.onClose}>
+    <div>
+    <h1>New Bot</h1>
+        <label>
+          New Bot's Name: <input name="postTitle" defaultValue="New Bot" className={classes.botname} />
+        </label>
+      </div>
+      <br></br>
+      <div className="container">
+        <h2>Upload PDF</h2>
+        <form className="form-group" onSubmit={handleSubmit}>
+          <Button onClick={handleClick}> Choose File</Button>
+          <input
+            type="file"
+            ref={hiddenFileInput}
+            style={{ display: "none" }}
+            className="form-control"
+            required
+            onChange={handleFileChange}
+          />
+          {pdfFileError && <div className="error-msg">{pdfFileError}</div>}
+          <br></br>
+        </form>
+        <h2>View PDF</h2>
+        <div className="pdf-container">
+          {/* show pdf conditionally (if we have one)  */}
+          {viewPdf && (
+            <>
+              <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
+                <Viewer
+                  fileUrl={viewPdf}
+                  plugins={[defaultLayoutPluginInstance]}
+                />
+              </Worker>
+            </>
+          )}
 
-            {!viewPdf && <>No pdf file choosen </>}
-          </div>
+          {!viewPdf && <>No pdf file choosen </>}
         </div>
-        <br></br>
-        <div className={classes.actions}>
-          <button className={classes["button--alt"]} onClick={props.onClose}>
-            Close
-          </button>
-        </div>
-      </Modal>
-    );
+      </div>
+      <br></br>
+      
+      <div className={classes.actions}>
+      <Button type="submit">Upload</Button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </Modal>
+  );
 
-//   return (
-//     <Modal onClose={props.onClose}>
-//       <div className="d-flex justify-content-center align-content-center file-upload">
-//         <div>
-//           <p className="book-upload">Upload a book to start swap</p>
-//           <div
-//             className="file-upload-area m-10"
-//             onDragOver={handleDragOver}
-//             onDrop={handleDrop}
-//           >
-//             <div
-//               className="card-body d-flex align-items-center justify-content-center m-2 scan-div"
-//               style={{ minHeight: "372px" }}
-//               draggable="true"
-//               onDragStart={handleDragStart}
-//             >
-//               <div className="file-upload-div">
-//                 <ul>
-//                   {files.map((file, index) => (
-//                     <li key={index}>{file.name}</li>
-//                   ))}
-//                 </ul>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </Modal>
-//   );
+  //   return (
+  //     <Modal onClose={props.onClose}>
+  //       <div className="d-flex justify-content-center align-content-center file-upload">
+  //         <div>
+  //           <p className="book-upload">Upload a book to start swap</p>
+  //           <div
+  //             className="file-upload-area m-10"
+  //             onDragOver={handleDragOver}
+  //             onDrop={handleDrop}
+  //           >
+  //             <div
+  //               className="card-body d-flex align-items-center justify-content-center m-2 scan-div"
+  //               style={{ minHeight: "372px" }}
+  //               draggable="true"
+  //               onDragStart={handleDragStart}
+  //             >
+  //               <div className="file-upload-div">
+  //                 <ul>
+  //                   {files.map((file, index) => (
+  //                     <li key={index}>{file.name}</li>
+  //                   ))}
+  //                 </ul>
+  //               </div>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </Modal>
+  //   );
 };
 
 export default UploadFileModal;
