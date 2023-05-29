92ef3a53323a6b33850d0445fd64005b73979396
ciaoosuuu <>
2023-05-30 06:04:24 +0800

    add

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ const LoginPage = () => {
   const logInHandler = useCallback(() => {
     setIsLoading(true);
 
-    fetch(`https://cors-anywhere.herokuapp.com/${SGID_BACKEND_URL}/api/auth-url`, {
+    fetch(`${SGID_BACKEND_URL}/api/auth-url`, {
       credentials: "include",
     })
       .then(async (r) => await r.json())
